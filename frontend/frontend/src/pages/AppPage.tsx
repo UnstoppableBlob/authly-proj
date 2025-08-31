e2ec4f398ef47f1dfb0d305458e9ddc4c338884b
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2025-08-31 14:56:31 -0700

    finished the qr generator app (basically done except scanning codes to login and final touches)

--- frontend/frontend/src/pages/AppPage.tsx
+++ frontend/frontend/src/pages/AppPage.tsx
@@ -1,10 +1,57 @@
-import { Button, VStack, HStack } from "@chakra-ui/react";
+import { Button, VStack, HStack, For, Spinner } from "@chakra-ui/react";
+import { Card, QrCode, SimpleGrid, Box, Input } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
 export default function AppPage() {
   const username = localStorage.getItem("username");
   const word1 = localStorage.getItem("word1");
   const word2 = localStorage.getItem("word2");
   const word3 = localStorage.getItem("word3");
+  const [code, setCode] = useState("");
+
+  if (!username || !word1 || !word2 || !word3) {
+    alert("You are not logged in!");
+    window.location.href = "/#/login";
+  }
+
+  const [history, setHistory] = useState<string[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  async function getHistory() {
+    // @app.route("/gethistory/<username>/<word1>/<word2>/<word3>")
+    const response = await fetch(
+      "http://127.0.0.1:5000/gethistory/" +
+        username +
+        "/" +
+        word1 +
+        "/" +
+        word2 +
+        "/" +
+        word3,
+      {
+        method: "GET",
+      }
+    );
+
+    const text = await response.text();
+
+    if (text === "user does not exist") {
+      alert("User does not exist!");
+      window.location.href = "/#/app";
+    } else if (text === "incorrect credentials") {
+      alert("Incorrect credentials!");
+      window.location.href = "/#/app";
+    } else {
+      const history = JSON.parse(text);
+      setHistory(history);
+      setLoading(false);
+      return history;
+    }
+  }
+
+  useEffect(() => {
+    getHistory();
+  }, []);
 
   async function logout() {
     localStorage.removeItem("username");
@@ -13,6 +60,43 @@ export default function AppPage() {
     localStorage.removeItem("word3");
     alert("Logged out!");
     window.location.href = "/#/";
+  }
+
+  async function onChange(e: any) {
+    setCode(e.target.value);
+  }
+
+  async function onSaveClick() {
+    const response = await fetch(
+      "http://127.0.0.1:5000/addhistory/" +
+        username +
+        "/" +
+        word1 +
+        "/" +
+        word2 +
+        "/" +
+        word3 +
+        "/" +
+        code,
+      {
+        method: "GET",
+      }
+    );
+
+    const text = await response.text();
+
+    if (text === "user does not exist") {
+      alert("User does not exist!");
+      window.location.href = "/#/app";
+    } else if (text === "incorrect credentials") {
+      alert("Incorrect credentials!");
+      window.location.href = "/#/app";
+    } else if (text === "added succesfully") {
+      alert("History added successfully!");
+      setHistory([...history, code]);
+    } else {
+      alert("Error: " + text);
+    }
   }
 
   return (
@@ -28,6 +112,60 @@ export default function AppPage() {
       <br />
       <br />
       <br />
+
+      <Card.Root>
+        <Card.Body>
+          <br />
+          <Input
+            type="text"
+            placeholder="Enter text"
+            onChange={onChange}
+            style={{ width: "80%", margin: "auto", textAlign: "center" }}
+          />
+          <br />
+          <QrCode.Root value={code} style={{ margin: "auto" }}>
+            <QrCode.Frame>
+              <QrCode.Pattern />
+            </QrCode.Frame>
+          </QrCode.Root>
+          <br />
+          <Button
+            onClick={onSaveClick}
+            style={{ margin: "auto", width: "100px" }}
+          >
+            Save
+          </Button>
+          <br />
+        </Card.Body>
+      </Card.Root>
+
+      <br />
+      <hr />
+      <br />
+      <h2 style={{ fontSize: "30px", fontWeight: "400" }}>Saved History</h2>
+      <br />
+
+      <SimpleGrid minChildWidth="100px" gap="20px">
+        <For
+          each={history.slice().reverse()}
+          fallback={
+            <div>{loading ? <Spinner /> : <p>No history found</p>}</div>
+          }
+        >
+          {(item) => (
+            <div>
+              <Box />
+              <QrCode.Root value={item} style={{ margin: "auto" }}>
+                <QrCode.Frame>
+                  <QrCode.Pattern />
+                </QrCode.Frame>
+              </QrCode.Root>
+              <p>{item}</p>
+              <Box />
+            </div>
+          )}
+        </For>
+      </SimpleGrid>
     </div>
   );
 }
